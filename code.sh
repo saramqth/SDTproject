5c63aa9d1ff6ec720fc7e602232856e831993f56
saramqth <120417618+saramqth@users.noreply.github.com>
2024-05-10 20:33:50 +0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,2 +1,5 @@
-read -p "Enter your name" name
-echo " Hello $name"
+echo "1. Check In\n 2. Make Reservation\n 3. Room Service\n 4. Check Out" 
+read -p "Enter the choic: " c
+if [ "$c"==1 ] then;
+read -p "Enter costumer name: " n
+grep "$n" Record.txt
